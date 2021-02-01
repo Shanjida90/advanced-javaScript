8599abc73ba54b8b33fe0eed6ef00eab80653bb0
unknown <aktershanjida414@gmail.com>
2021-02-01 12:13:13 -0800

    pushing file

---
+++ trueVsfalse.js
@@ -0,0 +1,32 @@
+// falsy 
+//  0,
+//  "",
+//  undefined,
+// null,
+// NaN ,
+// false
+
+//truthy
+
+// "0",' ',[], 
+
+
+
+ 
+
+
+
+
+
+
+
+// const age=5;
+const names=null;
+if (names) {
+console.log('condition is true');
+    
+}
+
+else{
+    console.log('condition is false');
+}
