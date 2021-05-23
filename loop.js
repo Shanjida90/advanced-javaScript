7f0a4baa0ac49a2eb887536f495bc2a1bf224c6c
unknown <aktershanjida414@gmail.com>
2021-05-22 23:28:49 -0700

    this is loop

---
+++ loop.js
@@ -0,0 +1,7 @@
+const array=[12,45,67,65,90,87,53,24]
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+// console.log(element)
+    const reslt=element*element;
+console.log(reslt);
+}
