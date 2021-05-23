3a60f379c486fa1b5b61f60f7edfcae021619223
unknown <aktershanjida414@gmail.com>
2021-05-23 16:18:24 -0700

    finiching loop

--- loop.js
+++ loop.js
@@ -1,7 +1,15 @@
-const array=[12,45,67,65,90,87,53,24]
+const array=[12,45,67,65,90,87,53,24];
+const output=[];
 for (let i = 0; i < array.length; i++) {
+
     const element = array[i];
 // console.log(element)
-    const reslt=element*element;
-console.log(reslt);
+    const result=element*element;
+output.push(result);
+
 }
+const result1=array.map(function square(element) {
+    return element*element;
+})
+console.log(result1)
+
